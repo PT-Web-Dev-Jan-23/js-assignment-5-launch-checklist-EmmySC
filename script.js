f2b80f092f70c3d757aa6734abec786400504b90
EmmyC <esc@Emilys-MBP-2.attlocal.net>
2023-03-14 15:28:55 -0500

    started over, was working in the LC101 template file of Launch Checklist. Pushing my code from that folder to this new graded assignment version.

--- script.js
+++ script.js
@@ -2,15 +2,37 @@
 
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+let list = document.getElementById("faultyItems"); //list.style.visibility = "hidden" --> default; 
+     let form = document.querySelector("form"); //accessing everything inside document's <form> & return references to elements via CSS selectors
    
-});
+   form.addEventListener("submit", function(event) { 
+        event.preventDefault();
+
+       let document = window.document
+       let pilot = document.querySelector("input[name=pilotName]"); //pilot.value
+       let copilot = document. querySelector("input[name=copilotName]"); //copilot.value
+       let fuelLevel = document.querySelector("input[name=fuelLevel]"); //fuelLevel.value
+       let cargoLevel = document.querySelector("input[name=cargoMass]"); //cargoLevel.value
+
+       formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value); //remove: .value
+
+    }) //form listener ending bracket & parentheses
+
+    //Set listedPlanetsResponse equal to the value returned by calling myFetch()
+      let listedPlanets;
+      let listedPlanetsResponse = myFetch(); 
+
+    listedPlanetsResponse.then(function (result) {
+      listedPlanets = result; 
+      console.log(listedPlanets); 
+    })
+     .then(function() {
+        console.log(listedPlanets);
+    //use pickPlanet() to randomly choose a planet from the list of planets and add that information to your destination
+      let selectedPlanet = pickPlanet(listedPlanets);
+      console.log(selectedPlanet);
+      
+      addDestinationInfo(window.document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image); 
+   })
+
+}); //window listener ending bracket & parentheses
